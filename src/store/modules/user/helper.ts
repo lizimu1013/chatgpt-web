9df7a0e7a9a0f1cd07eb7c6beaff29b27ff7227c
coda <coda@codadeMacBook-Pro.local>
2023-03-15 03:44:14 +0800

    chore: cover

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://doget-api.oopscloud.xyz/api/download?token=eyJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vbGl6aW11MTAxMy9jaGF0Z3B0LXdlYi9tYWluL3NyYy9hc3NldHMvYXZhdGFyLmpwZyJ9.5Pelnj8BT28RZvg5CoJ4OJnCts8ZVJQgr6B0jG2ETbk',
       name: '在非洲修基站',
-      description: '用爱发电：<a href="https://gi0.cn/sFY" class="text-blue-500" target="_blank" >Codaの小店</a>',
+      description: '<a href="https://gi0.cn/sFY" class="text-blue-500" target="_blank" >Codaの小店</a> <br/> <a href="https://jq.qq.com/?_wv=1027&k=HK6b0j2U" class="text-blue-500" target="_blank" >Codaの小群</a>',
     },
   }
 }
