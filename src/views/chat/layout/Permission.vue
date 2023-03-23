e1325828e07e90dab1a66bde2e82d84b676abf11
coda <coda@codadeMacBook-Pro.local>
2023-03-24 04:21:31 +0800

    chore: cover

--- src/views/chat/layout/Permission.vue
+++ src/views/chat/layout/Permission.vue
@@ -55,11 +55,14 @@ function handlePress(event: KeyboardEvent) {
     <div class="p-10 bg-white rounded dark:bg-slate-800">
       <div class="space-y-4">
         <header class="space-y-2">
-          <h2 class="text-2xl font-bold text-center text-slate-800 dark:text-neutral-200">
+          <!-- <h2 class="text-2xl font-bold text-center text-slate-800 dark:text-neutral-200">
             403
-          </h2>
+          </h2> -->
           <p class="text-base text-center text-slate-500 dark:text-slate-500">
-            {{ $t('请关注官方公众号，输入“key”获取免费密钥') }}
+            {{ $t('请长按或扫描二维码关注官方公众号，获取最新动态。') }}
+          </p>
+          <p class="text-base text-center text-slate-500 dark:text-slate-500">
+            {{ $t('公众号中输入“key”获取最新免费密钥') }}
           </p>
           <div class="container">
             <img src="https://mkdimg.oss-cn-shanghai.aliyuncs.com/img_for_typora_2023/%E6%89%AB%E7%A0%81_%E6%90%9C%E7%B4%A2%E8%81%94%E5%90%88%E4%BC%A0%E6%92%AD%E6%A0%B7%E5%BC%8F-%E7%99%BD%E8%89%B2%E7%89%88.png">
