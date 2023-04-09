71f06ec2a1453ba145c4cac673d2ee30807e9768
Liang Li <ll_faw@hotmail.com>
2023-04-09 21:27:02 +0800

    api

---
+++ src/views/admin.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { NButton } from 'naive-ui'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goHome() {
+  router.push('/')
+}
+</script>
+
+<template>
+  <div class="flex h-full">
+    <div class="px-4 m-auto space-y-4 text-center max-[400px]">
+      <NButton type="primary" @click="goHome">
+        Go to Home
+      </NButton>
+    </div>
+  </div>
+</template>
---
+++ src/views/refresh.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { NButton } from 'naive-ui'
+
+import { ref } from 'vue'
+import { fetchNewKey } from '@/api'
+
+const keys = ref([] as string[])
+
+const handleNewKey = async () => {
+  try {
+    const { data } = await fetchNewKey<string[]>()
+    keys.value = data
+  }
+  catch (error: any) {
+    console.error(error)
+  }
+}
+</script>
+
+<template>
+  <div class="flex h-full">
+    <div class="px-4 m-auto space-y-4 text-center max-[400px]">
+      <NButton type="primary" @click="handleNewKey">
+        新的Key
+      </NButton>
+      <br>
+      <code>{{ keys }}</code>
+    </div>
+  </div>
+</template>
